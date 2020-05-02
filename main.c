79114e436785ec57b0db8ca240c9074e8dd38bcf
serkviseal <serkviseal15@gmail.com>
2020-05-02 22:01:41 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("   ");
+    printf("*");
+    printf("*");
+    printf("*");
+    printf("   ");
+    printf("*");
+    printf("\n");
+    printf("*");
+    printf("    ");
+    printf("*");
+    printf("  ");
+    printf("*");
+    printf("    ");
+
+
+    return 0;
+}
